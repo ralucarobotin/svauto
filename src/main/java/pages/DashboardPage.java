1bb10607a56664beff4c82a2f37ab7ad4f130796
Catalin Mihale <c_cmihale@groupon.com>
2019-02-14 11:49:47 +0200

    Architecture update

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -4,27 +4,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import static helpers.WebElementHelper.waitForElementToAppear;
+import static helpers.WebElementHelper.areVisible;
 
 public class DashboardPage extends BasePage {
 
-  public static Header header;
+  public final static String URL="http://automationpractice.com";
 
   @FindBy(id = "homeslider")
   private WebElement carousel;
 
-  public DashboardPage(WebDriver driver){
-    super(driver);
-    header = new Header(driver);
-  }
-
-  public void login(String email, String password){
-    header.clickLoginButton();
-    new LoginPage(driver).login(email, password);
+  @Override
+  protected boolean isCurrent() {
+    return areVisible(carousel);
   }
 
   @Override
-  public void waitForPageToLoad(){
-    waitForElementToAppear(carousel);
+  protected boolean isValid() {
+    return areVisible(carousel);
+  }
+
+  public DashboardPage(WebDriver driver){
+    super(driver);
+  }
+
+  public void clickLoginButton(){
+      headerLoginButton.click();
+    }
+
+  public void open(){
+    openUrl(URL);
   }
 }
