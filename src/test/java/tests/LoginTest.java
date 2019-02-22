40e5e7aab604089fc1afae1fe50ee401b8d5d224
CosminBM <cosmincoco88@gmail.com>
2019-02-22 14:33:12 +0200

    First commit

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -17,10 +18,18 @@ public class LoginTest extends BaseTest {
 
   private String email;
   private String password;
+  private String invalidEmail;
+  private String invalidPassword;
+  private String emptyEmail;
+  private String emptyPassword;
 
   public LoginTest(){
     this.email = getPropertyFromAppProp("email");
     this.password = getPropertyFromAppProp("password");
+    this.invalidEmail = getPropertyFromAppProp("invalidEmail");
+    this.invalidPassword = getPropertyFromAppProp("invalidPassword");
+    this.emptyEmail = "";
+    this.emptyPassword = "";
   }
 
   @BeforeMethod
@@ -29,8 +38,8 @@ public class LoginTest extends BaseTest {
     this.loginPage = new LoginPage(getDriver());
     this.myAccountPage = new MyAccountPage(getDriver());
   }
-
-  @Test
+  //Happy flow login
+  @Test(priority = 5)
   public void testLogin(){
     this.dashboardPage.open();
     this.dashboardPage.verify();
@@ -40,5 +49,57 @@ public class LoginTest extends BaseTest {
     this.loginPage.login(email, password);
 
     this.myAccountPage.verify();
+  }
+  //Negative testing flow
+  @Test(priority = 3)
+  public void testInvalidEmail(){
+    this.dashboardPage.open();
+    this.dashboardPage.verify();
+    this.dashboardPage.clickLoginButton();
+
+    this.loginPage.verify();
+    this.loginPage.login(invalidEmail, password);
+    Assert.assertTrue(this.loginPage.errorMessage().isDisplayed());
+
+    this.loginPage.verify();
+  }
+
+  @Test(priority = 4)
+  public void testInvalidPassword(){
+    this.dashboardPage.open();
+    this.dashboardPage.verify();
+    this.dashboardPage.clickLoginButton();
+
+    this.loginPage.verify();
+    this.loginPage.login(email, invalidPassword);
+    Assert.assertTrue(this.loginPage.errorMessage().isDisplayed());
+
+    this.loginPage.verify();
+  }
+
+  @Test(priority = 1)
+  public void testEmptyEmail(){
+    this.dashboardPage.open();
+    this.dashboardPage.verify();
+    this.dashboardPage.clickLoginButton();
+
+    this.loginPage.verify();
+    this.loginPage.login(emptyEmail, password);
+    Assert.assertTrue(this.loginPage.errorMessage().isDisplayed());
+
+    this.loginPage.verify();
+  }
+
+  @Test(priority = 2)
+  public void testEmptyPassword(){
+    this.dashboardPage.open();
+    this.dashboardPage.verify();
+    this.dashboardPage.clickLoginButton();
+
+    this.loginPage.verify();
+    this.loginPage.login(email, emptyPassword);
+    Assert.assertTrue(this.loginPage.errorMessage().isDisplayed());
+
+    this.loginPage.verify();
   }
 }
