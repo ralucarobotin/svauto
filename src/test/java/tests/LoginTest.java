bebc3b3ca53464b27e85e4a2ba5aabb7b2104909
Madalin Cotetiu <cotetiumadalin@gmail.com>
2019-02-22 10:54:53 +0200

    added test data for negative tests in app.propreties, added negative testing scenarios for the login page, refactored the LoginTest to dinamicaly use test data in app.proprieties

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -19,8 +19,24 @@ public class LoginTest extends BaseTest {
   private String password;
 
   public LoginTest(){
-    this.email = getPropertyFromAppProp("email");
-    this.password = getPropertyFromAppProp("password");
+    this.email = "";
+    this.password = "";
+  }
+
+  public void setEmail(String email){
+    this.email = getPropertyFromAppProp(email);
+  }
+
+  public void setPassword(String password) {
+    this.password = getPropertyFromAppProp(password);
+  }
+
+  public void setEmail(){
+    this.email = "";
+  }
+
+  public void setPassword(){
+    this.password = "";
   }
 
   @BeforeMethod
@@ -32,6 +48,8 @@ public class LoginTest extends BaseTest {
 
   @Test
   public void testLogin(){
+    setEmail("email");
+    setPassword("password");
     this.dashboardPage.open();
     this.dashboardPage.verify();
     this.dashboardPage.clickLoginButton();
@@ -41,4 +59,54 @@ public class LoginTest extends BaseTest {
 
     this.myAccountPage.verify();
   }
+
+  @Test
+  public void testLoginInvalidEmailInvalidPassword(){
+    setEmail("invalidEmail");
+    setPassword("invalidPassword");
+
+    this.dashboardPage.open();
+    this.dashboardPage.verify();
+    this.dashboardPage.clickLoginButton();
+
+    this.loginPage.verify();
+    this.loginPage.login(email, password);
+
+    this.loginPage.verify();
+
+    //Add assertion for the error message
+  }
+
+  @Test
+  public void testLoginEmptyEmailEmptyPassword(){
+    setEmail();
+    setPassword();
+
+    this.dashboardPage.open();
+    this.dashboardPage.verify();
+    this.dashboardPage.clickLoginButton();
+
+    this.loginPage.verify();
+    this.loginPage.login(email, password);
+
+    this.loginPage.verify();
+
+    //Add assertion for the error message
+  }
+
+  @Test void testLoginWrongEmailWrongPassword(){
+    setEmail("wrongEmail");
+    setPassword("wrongPassword");
+
+    this.dashboardPage.open();
+    this.dashboardPage.verify();
+    this.dashboardPage.clickLoginButton();
+
+    this.loginPage.verify();
+    this.loginPage.login(email, password);
+
+    this.loginPage.verify();
+
+  //Add assertion for the error message
+}
 }
