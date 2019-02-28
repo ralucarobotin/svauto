d903592fbad26a5d3e0836e5bc8ae57e1fedd78b
CosminBM <cosmincoco88@gmail.com>
2019-02-28 17:42:23 +0200

    Added StringUtils.EMPTY in LoginTest and new element in DashboardPage

--- src/main/java/pages/CreateAnAccount.java
+++ src/main/java/pages/CreateAnAccount.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class CreateAnAccount {
+import static helpers.WebElementHelper.areVisible;
 
+public class CreateAnAccount extends BasePage {
+
+    //User's personal information
     @FindBy(id = "id_gender1")
     private WebElement genderMale;
 
@@ -13,18 +16,84 @@ public class CreateAnAccount {
     private WebElement genderFemale;
 
     @FindBy(id = "customer_firstname")
-    private WebElement userFirstName;
+    private WebElement customerFirstName;
 
     @FindBy(id = "customer_lastname")
-    private WebElement userLastName;
+    private WebElement customerLastName;
 
     @FindBy(id = "email")
-    private WebElement userEmail;
+    private WebElement email;
 
     @FindBy(id = "passwd")
-    private WebElement userPassword;
+    private WebElement password;
 
     @FindBy(id = "days")
-    private WebElement userDayOfBirth;
+    private WebElement days;
+
+    @FindBy(id = "months")
+    private WebElement months;
+
+    @FindBy(id = "years")
+    private WebElement years;
+
+    @FindBy(id = "newsletter")
+    private WebElement newsletter;
+
+    @FindBy(id = "optin")
+    private WebElement optin;
+
+    //User's Address
+    @FindBy(id = "firstname")
+    private WebElement firstName;
+
+    @FindBy(id = "lastname")
+    private WebElement lastName;
+
+    @FindBy(id = "company")
+    private WebElement company;
+
+    @FindBy(id = "address1")
+    private WebElement firstAddress;
+
+    @FindBy(id = "address2")
+    private WebElement secondAddress;
+
+    @FindBy(id = "city")
+    private WebElement city;
+
+    @FindBy(id = "id_state")
+    private WebElement state;
+
+    @FindBy(id = "postcode")
+    private WebElement postcode;
+
+    @FindBy(id = "id_country")
+    private WebElement country;
+
+    @FindBy(id = "other")
+    private WebElement otherInfo;
+
+    @FindBy(id = "phone")
+    private WebElement phone;
+
+    @FindBy(id = "phone_mobile")
+    private WebElement mobilePhone;
+
+    @FindBy(id = "submitAccount")
+    private WebElement registerButton;
+
+    public CreateAnAccount(WebDriver driver){
+        super(driver);
+    }
+
+    @Override
+    protected boolean isCurrent() {
+        return areVisible(genderMale);
+    }
+
+    @Override
+    protected boolean isValid() {
+        return areVisible(genderMale);
+    }
 
 }
