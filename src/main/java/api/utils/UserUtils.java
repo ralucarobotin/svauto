bfc7a82c1e003f8e7d5656a9454b3b943a072146
Raluca Robotin <rrobotin@signal.co>
2019-04-09 12:15:40 +0300

    Changed default user payload

--- src/main/java/api/utils/UserUtils.java
+++ src/main/java/api/utils/UserUtils.java
@@ -9,12 +9,12 @@ public class UserUtils {
   public static User generateDefaultPayload(){
     User user = new User();
     user.setId(new Random().nextInt(20));
-    user.setUsername(new String(new byte[10]));
-    user.setFirstName(new String(new byte[10]));
-    user.setLastName(new String(new byte[10]));
-    user.setEmail(new String(new byte[5]) + "@domain.com");
-    user.setPassword(new String(new byte[10]));
-    user.setPhone(new String(new byte[10]));
+    user.setUsername(new byte[10].toString());
+    user.setFirstName(new byte[10].toString());
+    user.setLastName(new byte[10].toString());
+    user.setEmail(new byte[5].toString() + "@domain.com");
+    user.setPassword(new byte[10].toString());
+    user.setPhone(new byte[10].toString());
     user.setUserStatus(new Random().nextInt(20));
 
     return user;
