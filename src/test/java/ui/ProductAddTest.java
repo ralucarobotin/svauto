709f334eaaa5fe022e2aabd125c11786f6f12549
GeluHai <haiduc.gelu@yahoo.com>
2019-11-09 17:08:57 +0200

    Latest fixes requested by code review for this branch.

--- src/test/java/ui/ProductAddTest.java
+++ src/test/java/ui/ProductAddTest.java
@@ -4,16 +4,21 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import uiPages.LoginPage;
 import uiPages.ProductPage;
 
-import static helpers.Utilities.getPropertyFromProductsProp;
+import static helpers.Utilities.getPropertyValue;
+import static helpers.Utilities.getPropertyFileContent;
 
 public class ProductAddTest extends BaseTest {
 
     private ProductPage productPage = null;
 
     private String specificProduct;
+    public static final String PRODUCT_FILE = "./products.properties";
+
+    public static String getPropertyFromProductsProp(String prop){
+        return getPropertyValue(getPropertyFileContent(PRODUCT_FILE), prop);
+    }
 
     public ProductAddTest() {
         this.specificProduct = getPropertyFromProductsProp("product3");
