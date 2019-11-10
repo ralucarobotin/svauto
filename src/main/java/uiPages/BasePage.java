39cc3f52ee0eda7ce76ba6d847d65355658dfb31
GeluHai <haiduc.gelu@yahoo.com>
2019-11-10 17:35:03 +0200

    Partially fixes #39.

--- src/main/java/uiPages/BasePage.java
+++ src/main/java/uiPages/BasePage.java
@@ -2,6 +2,8 @@ package uiPages;
 
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 
@@ -27,5 +29,4 @@ public class BasePage extends WebUIPage {
   protected boolean isValid() {
     return headerFooter.isValid();
   }
-
 }
