4e363cc93af6a8b71ae7cdff081b528bf2ab8d07
Radu Iman <radu.iman94@gmail.com>
2019-11-18 18:35:21 +0200

    [Issue #43] Create the 'submittingAnOrder' class

---
+++ src/test/java/ui/submittingAnOrder.java
@@ -0,0 +1,10 @@
+package ui;
+
+public class submittingAnOrder extends BaseTest {
+    /**
+     * 1. Add to cart some items
+     * 2. Place the order
+     * 3. Complete the checkout
+     * 4. Submit the order
+     */
+}
