07fe3bd492abc6ff3aee5280a8415a6edfee7ee4
Radu Iman <radu.iman94@gmail.com>
2019-11-19 18:15:21 +0200

    Started to implement the method to run before the test:  - beforeMethod()

--- src/test/java/ui/submittingAnOrder.java
+++ src/test/java/ui/submittingAnOrder.java
@@ -1,5 +1,13 @@
 package ui;
 
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.MyAccountPage;
+import uiPages.LoginPage;
+import uiPages.ProductPage;
+
+import static helpers.Utilities.getPropertyFromAppProp;
+
 public class submittingAnOrder extends BaseTest {
     /**
      * 1. Add to cart some items
@@ -7,4 +15,16 @@ public class submittingAnOrder extends BaseTest {
      * 3. Complete the checkout
      * 4. Submit the order
      */
+
+    @BeforeTest
+    public void BeforeMethod() {
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.open();
+        loginPage.login(getPropertyFromAppProp("emailValid"), getPropertyFromAppProp("passwordValid"));
+    }
+
+
+
+
+
 }
