ca6ad77c2a6c03e116107f0a84afc03532bb9324
Radu Iman <radu.iman@softvision.ro>
2019-11-21 23:03:57 +0200

    Add billing properties file and implemented the getPropertyFromBillingProp method

--- src/main/java/helpers/Utilities.java
+++ src/main/java/helpers/Utilities.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.WebDriverException;
 public class Utilities {
   public static final String APP_FILE = "./app.properties";
   public static final String PRODUCT_FILE = "./products.properties";
+  public static final String BILING_FILE = "./billing.properties";
   private static final Logger LOG = Logger.getLogger(Class.class.getName());
   private static final String SLEEP_INTERRUPTED = "Sleep interrupted!";
 
@@ -42,6 +43,10 @@ public class Utilities {
 
   public static String getPropertyFromProductsProp(String prop){
     return getPropertyValue(getPropertyFileContent(PRODUCT_FILE), prop);
+  }
+
+  public static String getPropertyFromBillingProp(String prop){
+    return getPropertyValue(getPropertyFileContent(BILING_FILE), prop);
   }
 
   public static Object executeScript(final WebDriver driver, final String script, final Object... args) {
