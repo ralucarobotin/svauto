0f86192af734083e76300dcbe982afafce75a4e8
unknown <vlad.bacia@P6294.ro.softvision.global>
2019-12-04 16:46:38 +0200

    Resolved conflicts and updated jackson-annotations to v2.9.9

--- src/main/java/api/domain/cart/items/BillingAddress.java
+++ src/main/java/api/domain/cart/items/BillingAddress.java
@@ -1,6 +1,9 @@
 package api.domain.cart.items;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.google.gson.annotations.SerializedName;
 
 public class BillingAddress {
 
@@ -8,22 +11,23 @@ public class BillingAddress {
     private String city;
     private String state;
     private String country;
-    @JsonProperty("postal_code")
-    private String postal_code;
+    @SerializedName("postal_code")
+    private String postalCode;
     private String phone;
     private String type;
     @JsonProperty("first_name")
     private String firstName;
     @JsonProperty("last_name")
     private String lastName;
-    @JsonProperty("address_1")
-    private String address_1;
-    @JsonProperty("address_2")
-    private String address_2;
+    @SerializedName("address_1")
+    private String address1;
+    @SerializedName("address_2")
+    private String address2;
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -31,6 +35,7 @@ public class BillingAddress {
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
@@ -38,6 +43,7 @@ public class BillingAddress {
     public String getState() {
         return state;
     }
+
     public void setState(String state) {
         this.state = state;
     }
@@ -45,20 +51,25 @@ public class BillingAddress {
     public String getCountry() {
         return country;
     }
+
     public void setCountry(String country) {
         this.country = country;
     }
 
+    @JsonGetter("postal_code")
     public String getPostalCode() {
-        return postal_code;
+        return postalCode;
     }
+
+    @JsonSetter("postal_code")
     public void setPostalCode(String postalCode) {
-        this.postal_code = postalCode;
+        this.postalCode = postalCode;
     }
 
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
@@ -66,35 +77,48 @@ public class BillingAddress {
     public String getType() {
         return type;
     }
+
     public void setType(String type) {
         this.type = type;
     }
 
+    @JsonGetter("first_name")
     public String getFirstName() {
         return firstName;
     }
+
+    @JsonSetter("first_name")
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    @JsonGetter("last_name")
     public String getLastName() {
         return lastName;
     }
+
+    @JsonSetter("last_name")
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    @JsonGetter("address_1")
     public String getAddress1() {
-        return address_1;
-    }
-    public void setAddress1(String address1) {
-        this.address_1 = address1;
+        return address1;
     }
 
-    public String getAddress2() {
-        return address_2;
+    @JsonSetter("address_1")
+    public void setAddress1(String address1) {
+        this.address1 = address1;
     }
+
+    @JsonGetter("address_2")
+    public String getAddress2() {
+        return address2;
+    }
+
+    @JsonSetter("address_2")
     public void setAddress2(String address2) {
-        this.address_2 = address2;
+        this.address2 = address2;
     }
 }
