2ef9b9a37e0d6fe55d40d5d784878afa4ad407f8
GeluHai <haiduc.gelu@yahoo.com>
2019-12-04 21:49:15 +0200

    Adding customer info to cart method and test

--- src/main/java/api/utils/CartUtils.java
+++ src/main/java/api/utils/CartUtils.java
@@ -1,6 +1,7 @@
 package api.utils;
 
 import api.domain.cart.items.BillingAddress;
+import api.domain.cart.items.Customer;
 import api.domain.cart.items.Skus;
 
 public class CartUtils {
@@ -17,6 +18,22 @@ public class CartUtils {
         item.setSkuId(skuId);
         item.setQuantity(quantity);
         return item;
+    }
+
+    /**
+     * Adds the customer information to the cart
+     *
+     * @param firstName the first name of the customer
+     * @param lastName the last name of the customer
+     * @param email the email address of the customer
+     * @return body with the customer info to be added to the cart
+     */
+    public static Customer addCustomerInfoToCart(String firstName, String lastName, String email) {
+        Customer customerInfo = new Customer();
+        customerInfo.setFirstName(firstName);
+        customerInfo.setLastName(lastName);
+        customerInfo.setEmail(email);
+        return customerInfo;
     }
 
     public static BillingAddress addBillingAddressToCart(String name, String city, String state, String country, String postalCode, String phone, String type, String address1, String address2) {
