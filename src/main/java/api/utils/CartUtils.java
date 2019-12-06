de3400bbf9a74ae0fce2f8087913270757ac5237
Ciprian Ciocan <pilu@Ciprians-MacBook-Pro.local>
2019-12-06 12:24:50 +0200

    Added files for Customer object and posting information about a new customer

--- src/main/java/api/utils/CartUtils.java
+++ src/main/java/api/utils/CartUtils.java
@@ -2,8 +2,13 @@ package api.utils;
 
 import api.domain.cart.items.BillingAddress;
 import api.domain.cart.items.Skus;
+import api.domain.checkout.cart.Cart;
+import api.wrappers.CartApiWrapper;
+import api.domain.customer.Customer;
 
 public class CartUtils {
+
+    private static final CartApiWrapper cartApiWrapper = new CartApiWrapper();
 
     /**
      * Adds an item to the card by it's SKU
@@ -19,6 +24,20 @@ public class CartUtils {
         return item;
     }
 
+    /**
+     * Method to generate valid billing address
+     *
+     * @param name
+     * @param city
+     * @param state
+     * @param country
+     * @param postalCode
+     * @param phone
+     * @param type
+     * @param address1
+     * @param address2
+     * @return {{@link BillingAddress}
+     */
     public static BillingAddress addBillingAddressToCart(String name, String city, String state, String country, String postalCode, String phone, String type, String address1, String address2) {
         BillingAddress billingAddress = new BillingAddress();
         billingAddress.setName(name);
@@ -33,4 +52,16 @@ public class CartUtils {
         return billingAddress;
     }
 
+    public static Cart createEmptyCart() {
+        Cart cart = new Cart();
+        return cart;
+    }
+
+    public static Customer createNewCustomer(String firstName, String lastName, String email) {
+        Customer customer = new Customer();
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setEmail(email);
+        return customer;
+    }
 }
